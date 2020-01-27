899e0571beb5fe5625d268b993edc6e0a83bb6e3
sophiakurihara <sophia.sudiacal@gmail.com>
2020-01-27 09:51:11 -0600

    test

---
+++ src/main/java/com/codeup/deimosspringblog/DeimosspringblogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.deimosspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeimosspringblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeimosspringblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/deimosspringblog/DeimosspringblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.deimosspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeimosspringblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
