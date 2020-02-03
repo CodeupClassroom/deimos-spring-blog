051eb1d208d5a5dd8ea544f9788788e583b9c2ba
Justin Reich <justinreich.dev@gmail.com>
2020-02-03 13:17:55 -0600

    feat: Add spring security

---
+++ src/main/java/com/codeup/deimosspringblog/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.codeup.deimosspringblog.controllers;
+
+import com.codeup.deimosspringblog.models.User;
+import com.codeup.deimosspringblog.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    private UserRepository users;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository users, PasswordEncoder passwordEncoder) {
+        this.users = users;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model){
+        model.addAttribute("user", new User());
+        return "users/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        users.save(user);
+        return "redirect:/login";
+    }
+}
